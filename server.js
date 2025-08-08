ab728a1f5c1ade4f68f44f8597bd1bbde7137baa
ahmed-elha <elhariri.ahmed59@gmail.com>
2025-08-09 00:20:38 +0200

    🚨 feat: add endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const config = require("./config/config");
+const metrics = require("./metrics/prometheus");
 
 // Init the express application
 const app = require("./config/express")();
@@ -26,4 +27,14 @@ console.log("Starting server on port 8080");
 // ADD /health endpoint
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+//Prometeus metric
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", metrics.register.contentType);
+    res.end(await metrics.register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
 });
